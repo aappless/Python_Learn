c4575b8569160fab1b83dd63577cf42a2212ace4
StevenLiu <aappless@gmail.com>
2024-04-23 20:19:37 +0800

    0423

---
+++ 4.IP138查詢/004app01.py
@@ -0,0 +1,37 @@
+#
+def get_mobile(phone):
+
+    # 發送=送請求地址
+    url=f'https://www.ip138.com/mobile.asp?mobile={phone}&action=mobile'
+    # 前面一段和抓小說一樣# 第一步 發送請求
+    # 安裝送網路請求的套件 CTRL+'
+    # pip install requests
+
+    import requests
+
+    # 偽裝成瀏灠器 使用PYTHON字典格式，其他語言稱MAP或是其他的，就是一個KEY 對一個VALUE
+    headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
+    # 發送請求
+    # resp=requests.get(url) 要加上 HEADER
+    resp=requests.get(url,headers=headers)
+
+    resp.encoding='utf-8'
+    # print(resp.text)
+
+    # 使用lxml 解析XPATH
+    # pip install lxml
+    from lxml import etree
+    e = etree.HTML(resp.text)
+    # datas = e.xpath('//tr/td[2]/*//text()')
+    datas = e.xpath('//tr/td[2]//a/text()')
+    #
+    # 以上XPATH ，原網站改了 用SPAN 不行的 XPATH 要改一改
+    # 註:text()方法
+    #
+    print(datas)
+    # NEXT 將以上傳成函式
+
+
+get_mobile(18811018888)
+
+# NEXT PYTHON WEB
